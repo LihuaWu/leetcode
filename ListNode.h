53a53188e1146f58e0a8aec3d14bda2b7b335146
LihuaWu <wulihua0@gmail.com>
2014-08-13 11:28:42 +0800

    list add

---
+++ ListNode.h
@@ -0,0 +1,15 @@
+#include <cstddef>
+#include <vector>
+
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode* next;	
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+ListNode* vector2List(const vector<int>& A); 
+void list2vector(ListNode* l, vector<int>& v);
+ListNode* add(ListNode* lhs, ListNode* rhs);
+ListNode* partialReverseList(ListNode(* head); 
--- listTest.cc
+++ listTest.cc
@@ -2,52 +2,9 @@
 
 #include <gtest/gtest.h>
 
+#include "ListNode.h"
+
 using namespace std;
-
-struct ListNode {
-	int val;
-	ListNode* next;	
-	ListNode(int x) : val(x), next(NULL) {}
-};
-
-ListNode* 
-add(ListNode* lhs, ListNode* rhs) {
-	ListNode dummy(0);
-	ListNode* prev = &dummy;
-	int carry = 0;
-	for(ListNode* l = lhs, *r = rhs;
-			l != NULL || r != NULL;
-			l = (l == NULL) ? NULL : l->next, 
-			r = (r == NULL) ? NULL : r->next,
-			prev = prev->next){
-		const int a = (l == NULL) ? 0 : l->val;
-		const int b = (r == NULL) ? 0 : r->val;
-
-		prev->next = new ListNode((a + b + carry) % 10);
-		carry = (a + b) / 10;
-	}
-	if(carry > 0){
-		prev->next = new ListNode(carry);
-	}
-	return dummy.next;
-}
-
-ListNode* vector2List(const vector<int>& A) {
-	int n = A.size();
-	ListNode dummy(0);
-	ListNode* prev = &dummy;
-	for(int i = 0; i < n; ++i, prev = prev->next) {
-		prev->next = new ListNode(A[i]);
-	}
-	return dummy.next;
-}
-
-void list2vector(ListNode* l, vector<int>& v){
-	while(l != NULL) {
-		v.push_back(l->val);
-		l = l->next;
-	}
-}
 
 TEST (ListAdd, Case1) {
 	int A[] = {1, 2, 3, 4, 5, 6, 7};
